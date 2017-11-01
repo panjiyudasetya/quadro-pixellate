38cb81a931bdcd985dd386b3f1e3f2319e3cc7e4
Panji Yudasetya Wiwaha <panji@sense-os.nl>
2017-11-01 13:42:01 +0700

    initialized app

---
+++ app/src/main/java/lunart/quadro/pixellatte/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package lunart.quadro.pixellatte
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
